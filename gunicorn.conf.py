c929781af7e06795b2a70c47c31ca09559da44b0
essattv <158786347+essattv@users.noreply.github.com>
2025-06-01 17:04:25 +0200

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,4 +1,6 @@
 bind = "0.0.0.0:10000"
-workers = 2
-threads = 4
-timeout = 120 
+workers = 1         # Mniej workerów = mniej RAM (duży wpływ)
+threads = 2         # Też mniej wątków
+timeout = 60        # Krótszy timeout zmniejsza szansę na zakleszczenia
+max_requests = 100  # Restart workera po 100 żądaniach
+max_requests_jitter = 10
